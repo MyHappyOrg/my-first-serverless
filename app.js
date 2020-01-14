ceb059fe998c80dcbef01648e0f3b69054235fb7
gokulram-sethuraman <gokulram.sethuraman@photoninfotech.net>
2020-01-14 17:16:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// const sls = require('serverless-http')
+// const app = express()
+// app.get('/', async (req, res, next) => {
+//   res.status(200).send('Hello Happy Updated123!' + process.env.BUCKET)
+// })
+// module.exports.server = sls(app)
+
+
+module.exports.server = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+      oldmessage: 'Hello Happy Updated123!' + process.env.BUCKET
+    }),
+  };
+
+  callback(null, response);
+};
